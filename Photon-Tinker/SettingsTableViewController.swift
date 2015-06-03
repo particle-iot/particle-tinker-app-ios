027de2ba75052853edf01f39b6217222f6095362
Ido Kleinman <idokleinman@gmail.com>
2015-06-02 18:10:51 -0700

    web view added

--- Photon-Tinker/SettingsTableViewController.swift
+++ Photon-Tinker/SettingsTableViewController.swift
@@ -55,63 +55,6 @@ class SettingsTableViewController: UITableViewController, UIPopoverPresentationC
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-      /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     
     override func viewWillAppear(animated: Bool) {
@@ -190,10 +133,12 @@ class SettingsTableViewController: UITableViewController, UIPopoverPresentationC
             
             }
         case 1: // documenation
+            var url : NSURL?
             switch indexPath.row
             {
             case 0:
                 println("documentation: app")
+                url = NSURL(string: "http://docs.particle.io")
             case 1:
                 println("documentation: setup")
             case 2:
@@ -202,6 +147,13 @@ class SettingsTableViewController: UITableViewController, UIPopoverPresentationC
                 println("default")
                 
             }
+        
+            
+            var webVC : WebViewController = self.storyboard!.instantiateViewControllerWithIdentifier("webview") as! WebViewController
+            webVC.link = url
+            webVC.linkTitle = tableView.cellForRowAtIndexPath(indexPath)?.textLabel?.text
+            
+            self.presentViewController(webVC, animated: true, completion: nil)
             
         case 2: // Support
             switch indexPath.row
---
+++ Photon-Tinker/WebViewController.swift
@@ -0,0 +1,104 @@
+//
+//  WebViewController.swift
+//  Particle
+//
+//  Created by Ido on 6/2/15.
+//  Copyright (c) 2015 Particle. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController, UIWebViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navBar.topItem?.title = self.linkTitle
+        self.navBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "Gotham-Book", size: 17)!]//,  NSForegroundColorAttributeName: UIColor.blackColor()]
+
+        
+        self.webView.loadRequest(NSURLRequest(URL: self.link!))
+        
+        self.webView.scalesPageToFit = true
+        self.webView.delegate = self;
+
+        
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var navBar: UINavigationBar!
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var webView: UIWebView!
+    var link : NSURL? = nil
+    var linkTitle : String? = nil
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func closeButtonTapped(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func startSpinner()
+    {
+        var hud : MBProgressHUD
+        
+        hud = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        hud.mode = .CustomView//.Indeterminate
+        hud.animationType = .ZoomIn
+        hud.labelText = "Loading"
+        hud.minShowTime = 0.4
+        
+        // prepare spinner view for first time populating of devices into table
+        var spinnerView : UIImageView = UIImageView(image: UIImage(named: "imgSpinner"))
+        spinnerView.frame = CGRectMake(0, 0, 37, 37);
+        spinnerView.contentMode = .ScaleToFill
+        var rotation = CABasicAnimation(keyPath:"transform.rotation")
+        rotation.fromValue = 0
+        rotation.toValue = 2*M_PI
+        rotation.duration = 1.0;
+        rotation.repeatCount = 1000; // Repeat
+        spinnerView.layer.addAnimation(rotation,forKey:"Spin")
+        
+        hud.customView = spinnerView
+    }
+    
+    func stopSpinner()
+    {
+        MBProgressHUD.hideHUDForView(self.view, animated: true)
+    }
+    
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError) {
+        self.stopSpinner()
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        self.startSpinner()
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        self.stopSpinner()
+        
+        let contentSize = self.webView.scrollView.contentSize;
+        let viewSize = self.view.bounds.size;
+        
+        let rw = viewSize.width / contentSize.width;
+        
+        self.webView.scrollView.minimumZoomScale = rw;
+        self.webView.scrollView.maximumZoomScale = rw;
+        self.webView.scrollView.zoomScale = rw;
+
+        
+    }
+    
+}
