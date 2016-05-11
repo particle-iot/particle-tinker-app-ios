1420a9f7753e47d76082a0092dc24ddccb1be050
Ido Kleinman <idokleinman@gmail.com>
2016-05-10 18:03:41 -0700

    new device types, new device info WIP

---
+++ DeviceInfoViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DeviceInfoViewController.swift
+//  Particle
+//
+//  Created by Ido Kleinman on 5/10/16.
+//  Copyright © 2016 spark. All rights reserved.
+//
+
+import Foundation
+
+class DeviceInfoViewController: UIViewController {
+    
+    
+    @IBOutlet weak var deviceIPAddressLabel: UILabel!
+    @IBOutlet weak var deviceStateLabel: UILabel!
+    @IBOutlet weak var firmwareVersionLabel: UILabel!
+    @IBOutlet weak var connectionLabel: UILabel!
+    @IBOutlet weak var lastHeardLabel: UILabel!
+    @IBOutlet weak var deviceTypeLabel: UILabel!
+    @IBOutlet weak var deviceIDLabel: UILabel!
+    
+    @IBAction func copyDeviceID(sender: AnyObject) {
+    }
+    
+    @IBOutlet weak var deviceNameLabel: UILabel!
+    @IBOutlet weak var IMEITitleLabel: UILabel!
+    @IBOutlet weak var IMEILabel: UILabel!
+
+    @IBOutlet weak var ICCIDTitleLabel: UILabel!
+    @IBOutlet weak var ICCIDLabel: UILabel!
+    
+    @IBOutlet weak var deviceDataTableView: UITableView!
+    
+    @IBOutlet weak var deviceImageView: UIImageView!
+    
+    @IBAction func backButtonTapped(sender: AnyObject) {
+    }
+    
+}
