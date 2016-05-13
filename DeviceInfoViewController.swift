9d526d81aa1584191da1dee0d80322d989593261
Ido Kleinman <idokleinman@gmail.com>
2016-05-12 18:54:57 -0700

    preliminary Device Info screen functional

--- DeviceInfoViewController.swift
+++ DeviceInfoViewController.swift
@@ -13,7 +13,7 @@ class DeviceInfoViewController: UIViewController, UITableViewDelegate {
     
     @IBOutlet weak var deviceIPAddressLabel: UILabel!
     @IBOutlet weak var deviceStateLabel: UILabel!
-    @IBOutlet weak var firmwareVersionLabel: UILabel!
+//    @IBOutlet weak var firmwareVersionLabel: UILabel!
     @IBOutlet weak var connectionLabel: UILabel!
     @IBOutlet weak var lastHeardLabel: UILabel!
     @IBOutlet weak var deviceTypeLabel: UILabel!
@@ -35,9 +35,52 @@ class DeviceInfoViewController: UIViewController, UITableViewDelegate {
     @IBOutlet weak var deviceImageView: UIImageView!
     
     @IBAction func backButtonTapped(sender: AnyObject) {
+        self.navigationController?.popViewControllerAnimated(true)
     }
     
+    
+    var deviceListViewController : DeviceListViewController?
+    
     var device : SparkDevice?
+    
+    override func viewDidLoad() {
+        let backgroundImage = UIImageView(image: UIImage(named: "imgTrianglifyBackgroundBlue")!)
+        backgroundImage.frame = UIScreen.mainScreen().bounds
+        backgroundImage.contentMode = .ScaleToFill;
+        self.view.addSubview(backgroundImage)
+        self.view.sendSubviewToBack(backgroundImage)
+
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        if self.device!.type != .Electron {
+            self.ICCIDTitleLabel.hidden = true
+            self.IMEITitleLabel.hidden = true
+            self.IMEILabel.hidden = true
+            self.ICCIDLabel.hidden = true
+        } else {
+            self.IMEILabel.text = self.device?.imei
+            self.ICCIDLabel.text = self.device?.lastIccid
+        }
+        
+        self.deviceIPAddressLabel.text = self.device?.lastIPAdress
+        self.lastHeardLabel.text = self.device?.lastHeard?.description.stringByReplacingOccurrencesOfString("+0000", withString: "") // process
+        self.deviceNameLabel.text = self.device?.name
+        self.deviceIDLabel.text = self.device?.id
+        self.connectionLabel.text = (self.device!.type == .Electron) ? "Cellular" : "Wi-Fi"
+
+        let deviceStateInfo = self.deviceListViewController!.getDeviceStateDescAndImage(self.device)
+        self.deviceStateLabel.text = deviceStateInfo.deviceStateText
+        self.deviceStateImageView.image = deviceStateInfo.deviceStateImage
+
+        let deviceInfo = self.deviceListViewController!.getDeviceNameAndImage(self.device)
+        self.deviceImageView.image = deviceInfo.deviceImage
+        self.deviceNameLabel.text = deviceInfo.deviceName
+
+        
+   
+        
+    }
     
     
     
