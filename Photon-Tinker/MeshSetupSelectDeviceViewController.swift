8b3c56f0d56153d1511a8c48e918f28dc1926c05
Ido Kleinman <idokleinman@gmail.com>
2018-06-25 16:23:54 -0700

    Add mesh devices gfx

--- Photon-Tinker/MeshSetupSelectDeviceViewController.swift
+++ Photon-Tinker/MeshSetupSelectDeviceViewController.swift
@@ -14,9 +14,20 @@ class MeshSetupSelectDeviceViewController : MeshSetupViewController, UITableView
     override func viewDidLoad() {
         deviceTypeTableView.delegate = self
         deviceTypeTableView.dataSource = self
+        self.navigationItem.rightBarButtonItem = self.cancelButton
     }
     
     var deviceTypes = [ "Xenon", "Argon", "Boron" ]
+    var deviceDescriptionTypes = [ "Mesh only", "Mesh and Wi-Fi gateway", "Mesh and Cellular gateway" ]
+    
+    let cancelButton: UIBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelButtonTapped(_:)))
+
+    
+    @objc func cancelButtonTapped(_ sender : Any) {
+        print("cancel pressed")
+        self.navigationController?.popViewController(animated: true)
+    }
+    
     @IBOutlet weak var deviceTypeTableView : UITableView!
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -31,18 +42,34 @@ class MeshSetupSelectDeviceViewController : MeshSetupViewController, UITableView
         }
     }
     
+    
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "deviceType")
         
         cell?.textLabel?.text = deviceTypes[indexPath.row]
+        cell?.detailTextLabel?.textColor = UIColor.darkGray
+        cell?.detailTextLabel?.text = deviceDescriptionTypes[indexPath.row]
         if (indexPath.row != 0) {
             cell?.textLabel?.textColor = UIColor.lightGray
+            cell?.detailTextLabel?.textColor = UIColor.lightGray
         }
+        
+        cell?.imageView?.image = UIImage.init(named: "img"+deviceTypes[indexPath.row])
+        
+        let itemSize = CGSize(width: 30, height: 64);
+        UIGraphicsBeginImageContextWithOptions(itemSize, false, UIScreen.main.scale);
+        let imageRect = CGRect(x: 0.0, y: 0.0, width: itemSize.width, height: itemSize.height);
+        cell?.imageView?.image!.draw(in: imageRect)
+        cell?.imageView?.image! = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext();
        
         return cell!
     }
     
-    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80.0
+    }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
