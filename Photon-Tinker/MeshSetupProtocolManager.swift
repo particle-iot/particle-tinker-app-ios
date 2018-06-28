d401979120acaf375dd79de1963c25b4c70fbd3d
Ido Kleinman <idokleinman@gmail.com>
2018-06-27 20:03:02 -0700

    WIP protocol manager

---
+++ Photon-Tinker/MeshSetupProtocolManager.swift
@@ -0,0 +1,160 @@
+//
+//  MeshSetupProtocolManager.swift
+//  Particle
+//
+//  Created by Ido Kleinman on 6/27/18.
+//  Copyright © 2018 spark. All rights reserved.
+//
+
+import UIKit
+
+class MeshSetupProtocolManager: NSObject {
+    //MARK: - View Properties
+    var bluetoothManager    : MeshSetupBluetoothManager?
+    
+    // Commissioning process data
+    var requestMessageId    : UInt16 = 0
+    var replyRequestTypeDict : [UInt16: ControlRequestMessageType]?
+    var waitingForReply     : Bool?
+    
+    // Commissioning data points
+    var networkInfo                     : Particle_Ctrl_Mesh_NetworkInfo?
+    var eui64                           : String?
+    var commissioningCredPassword       : String?
+    var commissioningCredPasswordTry    : String?
+    var joiningDeviceCred               : String?
+    
+    func sendRequestMessage(type : ControlRequestMessageType, payload : Data) {
+        
+        func showErrorDialog(message : String) {
+            let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "OK", style: .cancel) { (_) in }
+            alertController.addAction(cancelAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        
+        if let ble = self.bluetoothManager {
+            if !ble.isConnected() {
+                showErrorDialog(message: "BLE is not paired to mesh device")
+                return
+            }
+            
+            if let wfr = self.waitingForReply {
+                if wfr {
+                    showErrorDialog(message: "Waiting to hear back from device for a previously sent command, please wait")
+                }
+            }
+            
+            let requestMsg = RequestMessage(id: self.requestMessageId, type: type, size: UInt32(payload.count), data: payload)
+            
+            // add to state machine dictt to know which type of reply to deserialize
+            if self.replyRequestTypeDict != nil {
+                self.replyRequestTypeDict![requestMsg.id] = requestMsg.type
+            } else {
+                self.replyRequestTypeDict = [UInt16: ControlRequestMessageType]()
+                self.replyRequestTypeDict![requestMsg.id] = requestMsg.type
+            }
+            
+            DispatchQueue.main.async { // Correct
+                self.activityIndicator.startAnimating()
+                self.waitingForReply = true
+            }
+            self.requestMessageId = self.requestMessageId + 1
+            let sendBuffer = RequestMessage.serialize(requestMessage: requestMsg)
+            self.send(data: sendBuffer)
+        } else {
+            showErrorDialog(message: "BLE is not paired to mesh device")
+        }
+        
+    }
+    
+    func sendCreateNetwork(name : String, password : String) {
+        var requestMsgPayload = Particle_Ctrl_Mesh_CreateNetworkRequest()
+        requestMsgPayload.name = name
+        requestMsgPayload.password = password
+        self.commissioningCredPasswordTry = password
+        
+        guard let requestMsgPayloadData = try? requestMsgPayload.serializedData() else {
+            print("Could not serialize protobuf Particle_Ctrl_Mesh_CreateNetworkRequest message")
+            return
+        }
+        self.sendRequestMessage(type: ControlRequestMessageType.CreateNetwork, payload: requestMsgPayloadData)
+    }
+    
+    
+    func sendAuth(password : String) {
+        var requestMsgPayload = Particle_Ctrl_Mesh_AuthRequest()
+        requestMsgPayload.password = password
+        
+        guard let requestMsgPayloadData = try? requestMsgPayload.serializedData() else {
+            print("Could not serialize protobuf Particle_Ctrl_Mesh_AuthRequest message")
+            return
+        }
+        self.sendRequestMessage(type: .Auth, payload : requestMsgPayloadData)
+    }
+    
+    func sendStartCommissioner() {
+        let requestMsgPayload = Particle_Ctrl_Mesh_StartCommissionerRequest()
+        
+        guard let requestMsgPayloadData = try? requestMsgPayload.serializedData() else {
+            print("Could not serialize protobuf Particle_Ctrl_Mesh_StartCommissionerRequest message")
+            return
+        }
+        self.sendRequestMessage(type: .StartCommissioner, payload: requestMsgPayloadData)
+    }
+    
+    func sendPrepareJoiner(networkInfo : Particle_Ctrl_Mesh_NetworkInfo) {
+        var requestMsgPayload = Particle_Ctrl_Mesh_PrepareJoinerRequest()
+        requestMsgPayload.network = networkInfo
+        
+        guard let requestMsgPayloadData = try? requestMsgPayload.serializedData() else {
+            print("Could not serialize protobuf Particle_Ctrl_Mesh_PrepareJoinerRequest message")
+            return
+        }
+        self.sendRequestMessage(type: .PrepareJoiner, payload: requestMsgPayloadData)
+    }
+    
+    func sendAddJoiner(eui64 : String, password : String) {
+        var requestMsgPayload = Particle_Ctrl_Mesh_AddJoinerRequest()
+        requestMsgPayload.eui64 = self.eui64!
+        requestMsgPayload.password = self.joiningDeviceCred!
+        
+        guard let requestMsgPayloadData = try? requestMsgPayload.serializedData() else {
+            print("Could not serialize protobuf Particle_Ctrl_Mesh_AddJoinerRequest message")
+            return
+        }
+        self.sendRequestMessage(type: .AddJoiner, payload: requestMsgPayloadData)
+    }
+    
+    func sendJoinNetwork() {
+        let requestMsgPayload = Particle_Ctrl_Mesh_JoinNetworkRequest()
+        
+        guard let requestMsgPayloadData = try? requestMsgPayload.serializedData() else {
+            print("Could not serialize protobuf Particle_Ctrl_Mesh_JoinNetworkRequest message")
+            return
+        }
+        self.sendRequestMessage(type: .JoinNetwork, payload: requestMsgPayloadData)
+    }
+    
+    func sendStopCommissioner() {
+        let requestMsgPayload = Particle_Ctrl_Mesh_StopCommissionerRequest()
+        
+        guard let requestMsgPayloadData = try? requestMsgPayload.serializedData() else {
+            print("Could not serialize protobuf Particle_Ctrl_Mesh_StopCommissionerRequest message")
+            return
+        }
+        self.sendRequestMessage(type: .StopCommissioner, payload: requestMsgPayloadData)
+    }
+    
+    func sendLeaveNetwork() {
+        let requestMsgPayload = Particle_Ctrl_Mesh_LeaveNetworkRequest()
+        
+        guard let requestMsgPayloadData = try? requestMsgPayload.serializedData() else {
+            print("Could not serialize protobuf Particle_Ctrl_Mesh_LeaveNetworkRequest message")
+            return
+        }
+        self.sendRequestMessage(type: .LeaveNetwork, payload: requestMsgPayloadData)
+    }
+    
+    
+}
