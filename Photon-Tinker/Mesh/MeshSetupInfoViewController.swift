2f2bfc6593ba5a7a8d1c9332cf46cecf8a81bb62
Raimundas Sakalauskas <raimis@pinstudios.com>
2018-10-18 00:19:21 +0300

    add mesh setup info abstract view

---
+++ Photon-Tinker/Mesh/MeshSetupInfoViewController.swift
@@ -0,0 +1,71 @@
+//
+// Created by Raimundas Sakalauskas on 9/20/18.
+// Copyright © 2018 Particle. All rights reserved.
+//
+
+import UIKit
+
+class MeshSetupInfoViewController: MeshSetupViewController {
+
+    @IBOutlet weak var titleLabel: MeshLabel!
+
+    @IBOutlet var textLabels: [MeshLabel]!
+    @IBOutlet var textLabelValues: [String]!
+
+    @IBOutlet weak var continueButton: MeshSetupButton!
+
+    internal var setupMesh:Bool!
+    internal var callback: (() -> ())!
+
+    func setup(didFinishScreen: @escaping () -> (), setupMesh:Bool, networkName: String? = nil, deviceType: ParticleDeviceType? = nil, deviceName: String? = nil) {
+        self.callback = didFinishScreen
+
+        self.networkName = networkName
+        self.deviceType = deviceType
+        self.deviceName = deviceName
+
+        self.setupMesh = setupMesh
+    }
+
+    override func setStyle() {
+        titleLabel.setStyle(font: MeshSetupStyle.RegularFont, size: MeshSetupStyle.LargeSize, color: MeshSetupStyle.PrimaryTextColor)
+
+        for label in textLabels {
+            label.setStyle(font: MeshSetupStyle.RegularFont, size: MeshSetupStyle.RegularSize, color: MeshSetupStyle.PrimaryTextColor)
+        }
+    }
+
+    internal func setProgressLabelValues() {
+        let tfCount = textLabels.count
+        let valueCount = textLabelValues.count
+
+
+        for i in 0 ..< valueCount {
+            textLabels[i].isHidden = false
+            textLabels[i].text = textLabelValues[i]
+        }
+
+        //hide excessive text fields
+        for i in valueCount ..< tfCount {
+            textLabels[i].isHidden = true
+        }
+    }
+
+    internal func fadeContent() {
+        UIView.animate(withDuration: 0.25) { () -> Void in
+            self.titleLabel.alpha = 0.5
+            self.continueButton.alpha = 0.5
+
+            for textField in self.textLabels {
+                textField.alpha = 0.5
+            }
+        }
+    }
+
+    @IBAction func continuePressed(_ sender: Any) {
+        ParticleSpinner.show(view)
+        fadeContent()
+
+        callback()
+    }
+}
