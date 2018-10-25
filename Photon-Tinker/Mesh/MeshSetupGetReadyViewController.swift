2bcec8da983cab6df6a0fd04bbd7de2116116c0c
Raimundas Sakalauskas <raimis@pinstudios.com>
2018-10-25 20:02:53 +0300

    replace get ready screen labels with videos

--- Photon-Tinker/Mesh/MeshSetupGetReadyViewController.swift
+++ Photon-Tinker/Mesh/MeshSetupGetReadyViewController.swift
@@ -28,7 +28,7 @@ class MeshSetupGetReadyViewController: MeshSetupViewController, Storyboardable {
     @IBOutlet weak var ethernetToggleText: MeshLabel?
     
     @IBOutlet weak var continueButton: MeshSetupButton!
-    @IBOutlet weak var contentStackView: UIStackView!
+    //@IBOutlet weak var contentStackView: UIStackView!
     
     
     private var callback: ((Bool) -> ())!
@@ -41,7 +41,7 @@ class MeshSetupGetReadyViewController: MeshSetupViewController, Storyboardable {
     
     @IBAction func setupSwitchChanged(_ sender: Any) {
         continueButton.isUserInteractionEnabled = false
-        contentStackView.alpha = 0
+        //contentStackView.alpha = 0
         titleLabel.alpha = 0
         videoView.alpha = 0
         setViewContent()
@@ -60,7 +60,7 @@ class MeshSetupGetReadyViewController: MeshSetupViewController, Storyboardable {
         self.view.layoutIfNeeded()
 
         UIView.animate(withDuration: 0.125, delay: 0.125, options: [], animations: { () -> Void in
-            self.contentStackView.alpha = 1
+            //self.contentStackView.alpha = 1
             self.titleLabel.alpha = 1
             self.videoView.alpha = 1
         }, completion: { completed in
@@ -69,6 +69,7 @@ class MeshSetupGetReadyViewController: MeshSetupViewController, Storyboardable {
     }
 
     override func setStyle() {
+        videoView.backgroundColor = .clear
         videoView.layer.cornerRadius = 5
         videoView.clipsToBounds = true
 
@@ -77,7 +78,7 @@ class MeshSetupGetReadyViewController: MeshSetupViewController, Storyboardable {
 
 
         if (MeshScreenUtils.getPhoneScreenSizeClass() <= .iPhone5) {
-            contentStackView.spacing = 15
+            //contentStackView.spacing = 15
 
             titleLabel.setStyle(font: MeshSetupStyle.RegularFont, size: MeshSetupStyle.RegularSize, color: MeshSetupStyle.PrimaryTextColor)
 
@@ -90,7 +91,7 @@ class MeshSetupGetReadyViewController: MeshSetupViewController, Storyboardable {
             ethernetToggleTitle?.setStyle(font: MeshSetupStyle.BoldFont, size: MeshSetupStyle.SmallSize, color: MeshSetupStyle.PrimaryTextColor)
             ethernetToggleText?.setStyle(font: MeshSetupStyle.RegularFont, size: MeshSetupStyle.SmallSize, color: MeshSetupStyle.PrimaryTextColor)
         } else {
-            contentStackView.spacing = 20
+            //contentStackView.spacing = 20
 
             titleLabel.setStyle(font: MeshSetupStyle.RegularFont, size: MeshSetupStyle.LargeSize, color: MeshSetupStyle.PrimaryTextColor)
 
@@ -113,41 +114,51 @@ class MeshSetupGetReadyViewController: MeshSetupViewController, Storyboardable {
         ethernetToggleTitle?.text = MeshSetupStrings.GetReady.EthernetToggleTitle
         ethernetToggleText?.text = MeshSetupStrings.GetReady.EthernetToggleText
 
+        switch (self.deviceType!) {
+            case .xenon:
+                initializeVideoPlayerWithVideo(videoFileName: "xenon_power_on")
+            case .argon:
+                initializeVideoPlayerWithVideo(videoFileName: "argon_power_on")
+            case .boron:
+                initializeVideoPlayerWithVideo(videoFileName: "boron_power_on")
+            default:
+                //do nothing
+                break
+        }
+        
         view.setNeedsLayout()
         view.layoutIfNeeded()
-
-        initializeVideoPlayerWithVideo(videoFileName: "xenon_power_on")
     }
 
     private func setDefaultContent() {
         titleLabel.text = MeshSetupStrings.GetReady.Title
-        textLabel1.text = MeshSetupStrings.GetReady.Text1
-        textLabel2.text = MeshSetupStrings.GetReady.Text2
-        textLabel3.text = MeshSetupStrings.GetReady.Text3
-        textLabel4.text = MeshSetupStrings.GetReady.Text4
+//        textLabel1.text = MeshSetupStrings.GetReady.Text1
+//        textLabel2.text = MeshSetupStrings.GetReady.Text2
+//        textLabel3.text = MeshSetupStrings.GetReady.Text3
+//        textLabel4.text = MeshSetupStrings.GetReady.Text4
 
         replacePlaceHolderStrings()
-        hideEmptyLabels()
+//        hideEmptyLabels()
     }
 
     private func setEthernetContent() {
         titleLabel.text = MeshSetupStrings.GetReady.EthernetTitle
-        textLabel1.text = MeshSetupStrings.GetReady.EthernetText1
-        textLabel2.text = MeshSetupStrings.GetReady.EthernetText2
-        textLabel3.text = MeshSetupStrings.GetReady.EthernetText3
-        textLabel4.text = MeshSetupStrings.GetReady.EthernetText4
+//        textLabel1.text = MeshSetupStrings.GetReady.EthernetText1
+//        textLabel2.text = MeshSetupStrings.GetReady.EthernetText2
+//        textLabel3.text = MeshSetupStrings.GetReady.EthernetText3
+//        textLabel4.text = MeshSetupStrings.GetReady.EthernetText4
 
         replacePlaceHolderStrings()
-        hideEmptyLabels()
+//        hideEmptyLabels()
     }
 
 
-    internal func hideEmptyLabels() {
-        textLabel1.isHidden = (textLabel1.text?.count ?? 0) == 0
-        textLabel2.isHidden = (textLabel2.text?.count ?? 0) == 0
-        textLabel3.isHidden = (textLabel3.text?.count ?? 0) == 0
-        textLabel4.isHidden = (textLabel4.text?.count ?? 0) == 0
-    }
+//    internal func hideEmptyLabels() {
+//        textLabel1.isHidden = (textLabel1.text?.count ?? 0) == 0
+//        textLabel2.isHidden = (textLabel2.text?.count ?? 0) == 0
+//        textLabel3.isHidden = (textLabel3.text?.count ?? 0) == 0
+//        textLabel4.isHidden = (textLabel4.text?.count ?? 0) == 0
+//    }
 
     @IBAction func nextButtonTapped(_ sender: Any) {
         callback(self.setupSwitch.isOn)
@@ -156,18 +167,18 @@ class MeshSetupGetReadyViewController: MeshSetupViewController, Storyboardable {
     func initializeVideoPlayerWithVideo(videoFileName: String) {
         // Create a new AVPlayerItem with the asset and an
         // array of asset keys to be automatically loaded
-        let gatewayVideoString:String? = Bundle.main.path(forResource: "ethernet_featherwing_power_on", ofType: "mp4")
+        let gatewayVideoString:String? = Bundle.main.path(forResource: "featherwing_power_on", ofType: "mov")
         let gatewayVideoUrl = URL(fileURLWithPath: gatewayVideoString!)
         gatewayVideo = AVPlayerItem(url: gatewayVideoUrl)
 
-        let defaultVideoString:String? = Bundle.main.path(forResource: videoFileName, ofType: "mp4")
+        let defaultVideoString:String? = Bundle.main.path(forResource: videoFileName, ofType: "mov")
         let defaultVideoUrl = URL(fileURLWithPath: defaultVideoString!)
         defaultVideo = AVPlayerItem(url: defaultVideoUrl)
         
         self.videoPlayer = AVPlayer(playerItem: defaultVideo)
         layer = AVPlayerLayer(player: videoPlayer)
         layer!.frame = videoView.bounds
-        layer!.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        layer!.videoGravity = AVLayerVideoGravity.resizeAspect
 
         videoView.layer.addSublayer(layer!)
         setVideoLoopObserver()
