9b6e1ad592506394bcf4a84ed7846094a35c6240
Raimundas Sakalauskas <raimis@pinstudios.com>
2018-10-30 21:47:38 +0100

    proofread error messages for mesh setup

--- Photon-Tinker/Mesh/MeshSetupFlowHelperDefinitions.swift
+++ Photon-Tinker/Mesh/MeshSetupFlowHelperDefinitions.swift
@@ -186,53 +186,47 @@ enum MeshSetupFlowError: Error, CustomStringConvertible {
     case DeviceConnectToCloudTimeout
     case DeviceGettingClaimedTimeout
 
+
     public var description: String {
         switch self {
                 //these errors are handled instantly
-            case .FailedToUpdateDeviceOS : return "There was an error while performing DeviceOS update."
-            case .FailedToFlashBecauseOfTimeout : return "It seems that device has exited the listening mode. Please put it in listening mode again (blinking blue) and retry."
-
-            case .UnableToDownloadFirmwareBinary : return "Failed to download firmware update. Please try again later."
-            case .CannotAddGatewayDeviceAsJoiner : return "Support for adding multiple gateways to a single network is coming soon. Argons, Borons, and Xenons+Ethernet FeatherWings must be set up as a standalone device, or as the first gateway in a new mesh network."
-
-
-            case .WrongNetworkPassword : return "Provided password is incorrect."
-            case .WifiPasswordTooShort : return "Provided password is too short."
-            case .PasswordTooShort : return "Network password has to be between 6 and 16 characters."
+            case .FailedToUpdateDeviceOS : return "There was an error while performing a Device OS update."
+            case .FailedToFlashBecauseOfTimeout : return "It seems that your device has exited listening mode. Please put your device in listening mode (blinking blue) and retry."
+            case .UnableToDownloadFirmwareBinary : return "Failed to download the firmware update. Please try again later."
+            case .CannotAddGatewayDeviceAsJoiner : return "Support for adding multiple gateways to a single network is coming soon. Argons, Borons, and Xenons with Ethernet FeatherWings, must be set up as a standalone device or as the first gateway in a new mesh network."
+            case .WrongNetworkPassword : return "The password you entered is incorrect."
+            case .WifiPasswordTooShort : return "The password you entered is too short."
+            case .PasswordTooShort : return "Your network password must be between 6 and 16 characters."
             case .IllegalOperation : return "Illegal operation."
-            case .UnableToRenameDevice : return "Unable to rename device at this time. Please try again later."
-            case .NameTooShort : return "Device name cannot be empty."
-
+            case .UnableToRenameDevice : return "Unable to rename your device at this time. Please try again later."
+            case .NameTooShort : return "Your device name cannot be empty."
                 //user facing errors
-            case .FailedToActivateSim : return "SIM activation is taking longer than expected. Please retry your SIM activation. If you have retried many times, please contact support."
-            case .CCMissing : return "You need to add a credit card to your account to continue. Please visit https://console.particle.io/billing/edit-card to add a card and return here when you’re done."
+            case .FailedToActivateSim : return "SIM activation is taking longer than expected. Please retry your SIM activation. If you have retried multiple times, please contact support."
+            case .CCMissing : return "You need to add a credit card to your account to continue. Please visit https://console.particle.io/billing/edit-card to add a card and return here when you're done."
             case .UnableToGetPricingInformation : return "There was an error while retrieving pricing information. Please try again."
             case .UnableToGetSimStatus : return "There was an error while reading internal SIM card status. Please try again."
-            case .UnableToPublishDeviceSetupEvent : return "There was an error while notifying Particle Device Cloud about successful device setup. Please try again."
-            case .UnableToLeaveNetwork : return "There was an error while removing device from mesh network on Particle Device Cloud."
-            case .UnableToJoinNetwork : return "There was an error while adding device to mesh network on Particle Device Cloud."
-            case .UnableToJoinOldNetwork : return "Network you are trying to join, was created locally with test version of the app. Please create new network."
-            case .UnableToRetrieveNetworks : return "There was an error while accessing mesh network information on Particle Device Cloud."
-            case .UnableToCreateNetwork : return "There was an error while registering new network with Particle Device Cloud."
+            case .UnableToPublishDeviceSetupEvent : return "There was an error while notifying the Particle Device Cloud about successful device setup. Please try again."
+            case .UnableToLeaveNetwork : return "There was an error while removing your device from the mesh network on Particle Device Cloud."
+            case .UnableToJoinNetwork : return "There was an error while adding your device to mesh network on Particle Device Cloud."
+            case .UnableToJoinOldNetwork : return "The network you are trying to join was created locally with test version of the app. Please create new network."
+            case .UnableToRetrieveNetworks : return "There was an error while accessing your mesh network information on Particle Device Cloud."
+            case .UnableToCreateNetwork : return "There was an error while registering your new network with Particle Device Cloud."
             case .UnableToGenerateClaimCode : return "There was an error attempting to claim this device to your account."
             case .DeviceTooFar : return "Your mesh device is too far away from your phone. Please hold your phone closer and try again."
             case .FailedToStartScan : return "Bluetooth appears to be disabled on your phone. Please enable Bluetooth and try again."
-            case .FailedToScanBecauseOfTimeout : return "Unable to find your mesh device. Make sure the mesh device’s LED is blinking blue and that it’s not connected to any other devices."
+            case .FailedToScanBecauseOfTimeout : return "Unable to find your mesh device. Make sure the mesh device's LED is blinking blue and that it's not connected to any other devices."
             case .FailedToConnect : return "You phone failed to connect to your mesh device. Please try again."
             case .BluetoothDisabled : return "Bluetooth appears to be disabled on your phone. Please enable Bluetooth and try again."
-            case .BluetoothTimeout : return "Sending bluetooth message failed. Please try again."
-            case .BluetoothError : return "Something went wrong with Bluetooth. Please restart the setup process and try again."
+            case .BluetoothTimeout : return "Sending Bluetooth messages failed. Please try again."
+            case .BluetoothError : return "Something went wrong with Bluetooth. Please restart the set up process and try again."
             case .CommissionerNetworkDoesNotMatch : return "The assisting device is on a different mesh network than the one you are trying to join. Please make sure the devices are trying to use the same network."
             case .SameDeviceScannedTwice : return "This is the device that is being setup. Please scan the sticker of device that is on the mesh network you are trying to join."
-            case .WrongDeviceType : return "This is not {{device}}. Please scan {{device}} sticker or restart the setup and choose different device type."
-            case .FailedToObtainIp : return "Your device failed to obtain an IP address. Please make sure device has internet access."
-
-            case .BluetoothConnectionDropped : return "The Bluetooth connection was dropped unexpectedly. Please restart the setup and try again."
-
+            case .WrongDeviceType : return "This is not {{device}}. Please scan {{device}} sticker or restart the set up and choose different device type."
+            case .FailedToObtainIp : return "Your device failed to obtain an IP address. Please make sure your device has internet access."
+            case .BluetoothConnectionDropped : return "The Bluetooth connection was dropped unexpectedly. Please restart the set up and try again."
             case .DeviceIsNotAllowedToJoinNetwork : return "Your device was unable to join the network (NOT_ALLOWED). Please press try again."
             case .DeviceIsUnableToFindNetworkToJoin : return "Your device was unable to join the network (NOT_FOUND). Please press try again."
             case .DeviceTimeoutWhileJoiningNetwork : return "Your device was unable to join the network (TIMEOUT). Please press try again."
-
             case .DeviceConnectToCloudTimeout : return "Your device could not connect to Device Cloud. Please try again."
             case .DeviceGettingClaimedTimeout : return "Your device failed to be claimed. Please try again."
         }
