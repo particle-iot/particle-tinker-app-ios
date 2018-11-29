b314afc58f381d16326e29f372b3987b34b34b22
Raimundas Sakalauskas <raimis@pinstudios.com>
2018-11-29 23:44:41 +0200

    only output log to console for debug builds

--- Photon-Tinker/AppDelegate.swift
+++ Photon-Tinker/AppDelegate.swift
@@ -19,10 +19,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        ParticleLogger.setLoggerLevel(.debug)
+
         #if DEBUG
+            ParticleLogger.setLoggerLevel(.debug)
             //ParticleLogger.setIgnoreControls(["ParticleDevice", "BluetoothHandshakeManager", "BluetoothConnectionManager", "BluetoothConnection"])
         #endif
+    
         LogList.startLogging()
         LogList.clearStaleLogs()
 
