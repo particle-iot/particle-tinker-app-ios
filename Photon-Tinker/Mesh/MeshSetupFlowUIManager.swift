07396cbbedb861b8a427df62426bc65772a1b426
Raimundas Sakalauskas <raimis@pinstudios.com>
2018-12-17 16:54:24 +0200

    remove empty method override

--- Photon-Tinker/Mesh/MeshSetupFlowUIManager.swift
+++ Photon-Tinker/Mesh/MeshSetupFlowUIManager.swift
@@ -29,10 +29,6 @@ class MeshSetupFlowUIManager : UIViewController, Storyboardable, MeshSetupFlowMa
 
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
-
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
