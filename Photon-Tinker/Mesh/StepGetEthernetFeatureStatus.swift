8c55a9f8aa329aba2df86680f44b4c809d3407f9
Raimundas Sakalauskas <raimis@particle.io>
2019-04-02 23:11:57 +0300

    add get ethernet status step

---
+++ Photon-Tinker/Mesh/StepGetEthernetFeatureStatus.swift
@@ -0,0 +1,54 @@
+//
+// Created by Raimundas Sakalauskas on 2019-03-04.
+// Copyright (c) 2019 spark. All rights reserved.
+//
+
+import Foundation
+
+class StepGetEthernetFeatureStatus: MeshSetupStep {
+
+    override func start() {
+        guard let context = self.context else {
+            return
+        }
+
+        if context.targetDevice.ethernetDetectionFeature == nil {
+            self.getFeatureFlag()
+        } else {
+            self.stepCompleted()
+        }
+    }
+
+    private func getFeatureFlag() {
+        guard let context = self.context else {
+            return
+        }
+
+        context.targetDevice.transceiver!.sendGetFeature(feature: .ethernetDetection) { [weak self, weak context] result, enabled in
+            guard let self = self, let context = context, !context.canceled else {
+                return
+            }
+
+            context.targetDevice.ethernetDetectionFeature = enabled!
+            self.log("targetDevice.sendGetFeature: \(result.description()) enabled: \(enabled as Optional)")
+
+            if (result == .NONE) {
+                self.start()
+            } else if (result == .NOT_SUPPORTED) {
+                self.stepCompleted()
+            } else {
+                self.handleBluetoothErrorResult(result)
+            }
+        }
+    }
+
+    override func rewindTo(context: MeshSetupContext) {
+        super.rewindTo(context: context)
+
+        guard let context = self.context else {
+            return
+        }
+
+        context.targetDevice.ethernetDetectionFeature = nil
+    }
+}
