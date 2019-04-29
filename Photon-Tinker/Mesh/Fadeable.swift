01fdc072ca561dd616e9122a60303fac1d53401a
Raimundas Sakalauskas <raimis@particle.io>
2019-04-29 17:10:48 +0300

    expand Fadable to support unanimated fade in

--- Photon-Tinker/Mesh/Fadeable.swift
+++ Photon-Tinker/Mesh/Fadeable.swift
@@ -9,37 +9,47 @@ protocol Fadeable: class where Self: UIViewController {
     var isBusy: Bool { get set }
     var viewsToFade: [UIView]? { get }
 
-    func fade()
+    func fade(animated: Bool)
     func resume(animated: Bool)
 }
 
 extension Fadeable {
 
-    func fade() {
+    func fade(animated: Bool = true) {
+        fadeContent(animated: animated)
+    }
+
+    func resume(animated: Bool = true) {
+        unfadeContent(animated: animated)
+    }
+
+    func fadeContent(animated: Bool) {
         self.isBusy = true
-
         ParticleSpinner.show(view)
-        fadeContent()
-    }
 
-    func resume(animated: Bool) {
-        ParticleSpinner.hide(view, animated: animated)
-        unfadeContent(animated: true)
-
-        self.isBusy = false
-    }
-
-    func fadeContent() {
-        UIView.animate(withDuration: 0.25) { () -> Void in
+        if (animated) {
+            UIView.animate(withDuration: 0.25) { () -> Void in
+                if let viewsToFade = self.viewsToFade {
+                    for childView in viewsToFade {
+                        childView.alpha = 0.5
+                    }
+                }
+            }
+        } else {
             if let viewsToFade = self.viewsToFade {
                 for childView in viewsToFade {
                     childView.alpha = 0.5
                 }
             }
+
+            self.view.setNeedsDisplay()
         }
     }
 
     func unfadeContent(animated: Bool) {
+        ParticleSpinner.hide(view, animated: animated)
+        self.isBusy = false
+
         if (animated) {
             UIView.animate(withDuration: 0.25) { () -> Void in
                 if let viewsToFade = self.viewsToFade {
