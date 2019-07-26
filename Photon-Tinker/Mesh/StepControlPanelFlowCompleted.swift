56925a417f5c8830d4831b84300f46920ead1322
Raimundas Sakalauskas <raimis@particle.io>
2019-07-26 17:22:27 +0300

    for flow complete step, mark step as complete before hitting callback to allow flow switching in the callback without triggering a "flow is broken" error.

--- Photon-Tinker/Mesh/StepControlPanelFlowCompleted.swift
+++ Photon-Tinker/Mesh/StepControlPanelFlowCompleted.swift
@@ -11,7 +11,7 @@ class StepControlPanelFlowCompleted: MeshSetupStep {
             return
         }
 
-        context.delegate.meshSetupDidCompleteControlPanelFlow(self)
         self.stepCompleted()
+        context.delegate.meshSetupDidCompleteControlPanelFlow(self)
     }
 }
