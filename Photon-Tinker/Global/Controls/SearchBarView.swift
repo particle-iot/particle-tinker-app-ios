88f4c67c32fadecdf92822f22bd607ba04aea60d
Raimundas Sakalauskas <raimis@particle.io>
2019-08-07 16:26:21 +0300

    add SearcBarView

---
+++ Photon-Tinker/Global/Controls/SearchBarView.swift
@@ -0,0 +1,170 @@
+//
+// Created by Raimundas Sakalauskas on 2019-08-07.
+// Copyright (c) 2019 Particle. All rights reserved.
+//
+
+import Foundation
+
+protocol SearchBarViewDelegate: class {
+    func searchBarTextDidChange(searchBar: SearchBarView, text: String?)
+
+    func searchBarDidBeginEditing(searchBar: SearchBarView)
+    func searchBarDidEndEditing(searchBar: SearchBarView)
+}
+
+class SearchBarView: UIView, UITextFieldDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var inputText: CustomizableTextField!
+    
+    @IBOutlet weak var cancelButton: UIButton!
+
+    weak var delegate: SearchBarViewDelegate?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        backgroundColor = UIColor.clear
+
+        inputText.font = UIFont(name: ParticleStyle.RegularFont, size: CGFloat(ParticleStyle.RegularSize))
+        inputText.textColor = ParticleStyle.PrimaryTextColor
+        inputText.borderStyle = .none
+        inputText.clearButtonMode = .whileEditing
+        inputText.tintColor = ParticleStyle.ButtonColor
+        inputText.placeholder = ""
+        inputText.placeholderColor = ParticleStyle.SecondaryTextColor
+        inputText.clearButtonTintColor = ParticleStyle.ButtonColor
+        inputText.text = ""
+        inputText.delegate = self
+
+        cancelButton.alpha = 0
+        cancelButton.isHidden = true
+        cancelButton.isEnabled = false
+    }
+
+    override func willMove(toSuperview newSuperview: UIView?) {
+        super.willMove(toSuperview: newSuperview)
+
+        NotificationCenter.default.removeObserver(self)
+
+        if let superview = newSuperview {
+            NotificationCenter.default.addObserver(self, selector: #selector(textChanged), name: Notification.Name.UITextFieldTextDidChange, object: self.inputText)
+        }
+    }
+
+    @IBAction func cancelClicked() {
+        inputText.text = ""
+
+        self.endEditing(true)
+        hideCancel()
+
+        textChanged()
+    }
+
+    private func showCancel() {
+        cancelButton.alpha = 0
+        cancelButton.isHidden = false
+        cancelButton.isEnabled = true
+
+        UIView.animate(withDuration: 0.25) {
+            self.layoutIfNeeded()
+            self.cancelButton.alpha = 1
+        }
+    }
+
+    private func hideCancel() {
+        cancelButton.alpha = 1
+        cancelButton.isEnabled = false
+
+        UIView.animate(withDuration: 0.25, animations: {
+            self.layoutIfNeeded()
+            self.cancelButton.alpha = 0
+
+        }, completion: { b in
+            self.cancelButton.isHidden = true
+        })
+    }
+
+    @objc
+    public func textChanged() {
+        if var searchTerm = inputText.text {
+            self.delegate?.searchBarTextDidChange(searchBar: self, text: searchTerm.trimmingCharacters(in: .whitespacesAndNewlines))
+        } else {
+            self.delegate?.searchBarTextDidChange(searchBar: self, text: nil)
+        }
+    }
+
+    //MARK: TextField Delegate
+    public func textFieldDidBeginEditing(_ textField: UITextField) {
+        delegate?.searchBarDidBeginEditing(searchBar: self)
+        showCancel()
+    }
+
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+
+    public func textFieldDidEndEditing(_ textField: UITextField) {
+        if let value = textField.text {
+            textField.text = value.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+        }
+        delegate?.searchBarDidEndEditing(searchBar: self)
+
+        if (textField.text?.isEmpty ?? false) {
+            hideCancel()
+        }
+    }
+}
+
+class CustomizableTextField: UITextField {
+    var placeholderColor: UIColor? = nil {
+        didSet {
+            evalPlaceholderColor()
+        }
+    }
+
+    override var placeholder: String? {
+        get {
+            return super.placeholder
+        }
+        set {
+            super.placeholder = newValue
+            evalPlaceholderColor()
+        }
+    }
+
+    var clearButtonTintColor: UIColor? = nil {
+        didSet {
+            evalClearButtonTintColor()
+        }
+    }
+
+    func evalPlaceholderColor() {
+        if let placeholder = placeholder, let placeHolderColor = placeholderColor {
+            attributedPlaceholder = NSAttributedString.init(string: placeholder, attributes: [
+                NSAttributedStringKey.foregroundColor: placeHolderColor,
+                NSAttributedStringKey.font: UIFont(name: ParticleStyle.RegularFont, size: CGFloat(ParticleStyle.RegularSize))!
+            ])
+        }
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        evalClearButtonTintColor()
+    }
+
+    private func evalClearButtonTintColor() {
+        for view in subviews as [UIView] {
+            if view is UIButton {
+                let button = view as! UIButton
+
+                if let clearButtonTintColor = clearButtonTintColor {
+                    let tintedImage = UIImage(named: "ClearIcon")!.image(withColor: clearButtonTintColor)
+
+                    button.setImage(tintedImage, for: .normal)
+                    button.setImage(tintedImage, for: .highlighted)
+                }
+            }
+        }
+    }
+}
