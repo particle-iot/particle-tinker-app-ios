ce00012d7fe3408a633b101671449884cac4e26c
Raimundas Sakalauskas <raimis@particle.io>
2019-08-28 14:56:39 +0300

    add UIView extension to recursively replace all strings in UIView on awakeFromNib

--- Photon-Tinker/TinkerStrings.swift
+++ Photon-Tinker/TinkerStrings.swift
@@ -5,6 +5,30 @@
 
 import Foundation
 
+extension UIView {
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+
+        self.replaceTinkerStrings()
+    }
+
+    open func replaceTinkerStrings() {
+        let subviews = self.subviews
+
+        for subview in subviews {
+            if subview is UILabel {
+                let label = subview as! UILabel
+                label.text = label.text?.tinkerLocalized()
+            } else if (subview is ParticleButton) {
+                let button = subview as! ParticleButton
+                button.setTitle(button.currentTitle?.tinkerLocalized(), for: .normal)
+            } else if (subview is UIView) {
+                subview.replaceTinkerStrings()
+            }
+        }
+    }
+}
+
 extension String {
     func tinkerLocalized() -> String {
         return NSLocalizedString(self, tableName: "TinkerStrings", comment: "")
