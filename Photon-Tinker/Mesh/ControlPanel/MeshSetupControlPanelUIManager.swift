270a38597bce35940993513d87d3bd55ba228dfb
Raimundas Sakalauskas <raimis@particle.io>
2019-10-09 22:38:53 +0200

    only use mobile secret assertion for gen 3 devices

--- Photon-Tinker/Mesh/ControlPanel/MeshSetupControlPanelUIManager.swift
+++ Photon-Tinker/Mesh/ControlPanel/MeshSetupControlPanelUIManager.swift
@@ -21,7 +21,7 @@ class MeshSetupControlPanelUIManager: MeshSetupUIBase {
         self.log("Setting device: \(device)")
         if let serial = device.serialNumber, let mobileSecret = device.mobileSecret {
             self.targetDeviceDataMatrix = MeshSetupDataMatrix(serialNumber: device.serialNumber!, mobileSecret: device.mobileSecret!, deviceType: device.type)
-        } else {
+        } else if (device.is3rdGen()) {
             fatalError("Device Serial Number (\(device.serialNumber)) or Mobile Secret (\(device.mobileSecret)) is missing.")
         }
 
